98d7e8f2e16a45746a10f3dcfc7bf4b4c9669f8b
Jimmy Osma <josma109@gmail.com>
2023-08-31 20:43:44 -0500

    astro init commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+        // '/src/**/*/*.{astro,html,js,jsx}',
+        './src/pages/*.{astro,html,js}',
+        './src/components/*.{astro,html,js}',
+        
+    ],
+    // theme: {
+    //     extend: {
+
+    //     }
+    // },
+
+}
